23d7546caa04f1f747bff69feae637ced3f0cfdd
Nut <natjamee.to@mail.wu.ac.th>
2025-02-20 11:21:02 +0700

    Delete """

--- Send/Sine.py
+++ Send/Sine.py
@@ -26,24 +26,3 @@ plt.ylabel("Amplitude")
 plt.grid(True)
 plt.show()
 
-
-
-
-"""
-
-#Importing required library
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-# Creating x axis with range and y axis with Sine
-# Function for Plotting Sine Graph
-x = np.arange(0, 5*np.pi, 0.1)
-y = np.sin(x)
-
-# Plotting Sine Graph
-plt.plot(x, y, color='green')
-plt.show()
-
-"""
-
