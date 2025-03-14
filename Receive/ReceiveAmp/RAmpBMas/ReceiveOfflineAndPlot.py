e0dc9ada0c511534becb68696d083d6b3c5cb410
NatjameeTH <natjamee.to@mail.wu.ac.th>
2025-03-14 15:16:09 +0700

    สร้างไฟล์สำหรับรับสัญญาณจาก Brainmaster

---
+++ Receive/ReceiveAmp/RAmpBMas/ReceiveOfflineAndPlot.py
@@ -0,0 +1,2 @@
+# รับสัญญาณ จาก Brainmaster Discovery24E  (LSL: LabRecorder)
+# Plot กราฟ แบบ Offline ด้วย MatPlotlib
---
+++ Receive/ReceiveAmp/RAmpBMas/ReceiveRealtimePyq.py
@@ -0,0 +1,2 @@
+# รับสัญญาณ จาก Brainmaster Discovery24E (LSL: LabRecorder)
+# Plot กราฟ แบบ Real-time ด้วย pyqtgraph
