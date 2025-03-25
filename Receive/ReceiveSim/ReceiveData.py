f10bbcace21f4ec5590eca9b910d5a1ef9f4d760
NatjameeTH <Natjamee.to@mail.wu.ac.th>
2025-03-25 14:52:16 +0700

    Update comment

--- Receive/ReceiveSim/ReceiveData.py
+++ Receive/ReceiveSim/ReceiveData.py
@@ -1,5 +1,5 @@
 # รับสัญญาณ จาก OpenViBe Acquisition Server (LSL: LabRecorder)
-# โดยใช้ Driver -> Generic Oscillator
+# โดยใช้ Driver -> Generic Oscillator ใช้สร้างสัญญาณคลื่นแบบจำลอง  
 
 
 import numpy as np
